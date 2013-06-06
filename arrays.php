f57863b086f77405b8f1fcf47e24768a6d5dc4d7
tykayn <tykayn@gmail.com>
2013-06-06 22:47:35 +0200

    planning mangastyl a ses débuts dans un fichier arrays dédié

---
+++ arrays.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$mangastyl = array(
+    '2004-05-01' => 'idée du fanzine Mangastyl',
+    '2004-05-01,2005-11-01' => 'construction du premier numéro',
+    '2004-11-01,2006-06-01' => 'construction du deuxième numéro',
+    '2004-03-01,2006-11-01' => 'construction du troisième numéro',
+    '2005-11-19,2005-11-20' => 'convention Anim Est 2005 à Nancy avec Waltch, Jp-furia,Sneups,tasse,amelitha',
+    '2006-02-18,2006-02-19' => 'convention Fan festival 2006',
+    '2006-07-01,2006-07-04' => 'convention Japan Expo 2006, à Villepinte',
+    '2006-07-02' => 'réimpression en urgence de 50 numéros 2 du jour au lendemain',
+    '2006-07-01,2006-07-04' => 'convention Manga Expo 2006, Mangastyl',
+    '2005-11-01' => 'impressions de 50 numéros 1',
+    '2006-06-15' => 'impressions de 50 numéros 2 du fanzine qui déchire du boudin',
+    '2006-09-01' => 'réimpression de 25 n°1 et 25 n°2',
+    '2005-08-01' => 'Deviantart Mangastyl est créé',
+    '2005-02-01' => 'l\'association loi 1901 Mangastyl est créé',
+    '2006-01-01' => 'début des dédicaces par Tykayn en vente par correspondance',
+)
+?>
