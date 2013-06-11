5449c3239e2bb9f650e5867d0b9849ef1bed11a6
tykaynGT <tykayngt@gmail.com>
2013-06-11 12:31:33 +0200

    conversion en jours mois années

--- ng/js/controllers.js
+++ ng/js/controllers.js
@@ -1,19 +1,42 @@
  angular.module("tk.controllers", []).controller('indexCtrl',['$scope' , function($scope){
     console.log("yeoooooo");
-    $scope.calcul = function(obj){
-       var result =  obj / $scope.brutannuel / 12 * 0.77  // mois
-        if( result < 12){
-            return result + ' mois';
+    d = new Date()
+    $scope.ans = 0
+    $scope.futur = d.getFullYear() + $scope.ans 
+    
+    $scope.calcul =     function(obj,brutannuel){
+            console.log("brutannuel" + brutannuel);
+      var result =   obj / brutannuel * 0.77  ;
+      console.log("result " + result+' mois');
+      console.log("result " + result*31 +' jours');
+        if( result < 0.5){
+         var h = result *8*31
+            return h.toFixed(2)  + ' heures';
+        }
+        else
+        if( result < 1){
+         var jours = result *31
+            return jours.toFixed(2)  + ' jours';
+        }
+        else if( result < 12){
+            return result.toFixed(2)  + ' mois';
         }
         else if(result > 11){
-            return (result / 12) + ' ans';
+             var ans = result / 12 
+         $scope.futur =  d.getFullYear() + ans.toFixed(1)
+            return ans.toFixed(2) + ' ans';
+            
         }
         else{
             return 'meeeh'
         }
+        return 'heh'
     }
+    
+    
+    $scope.profil = 1398.37;
     $scope.test = 'hello le test';
-    $scope.brutannuel = 40000;
+    $scope.brutannuel = 38000;
     $scope.prixObjet = 100;
     $scope.objets = 20;
 }])
