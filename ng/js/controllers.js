5e2b6f56e96255350b12823a762b4f0533632ba4
tykaynGT <tykayngt@gmail.com>
2013-06-11 10:30:01 +0200

    ajout de sélect page salaire

--- ng/js/controllers.js
+++ ng/js/controllers.js
@@ -1,7 +1,19 @@
  angular.module("tk.controllers", []).controller('indexCtrl',['$scope' , function($scope){
-    
-
-    $scope.test = 'hello';
+    console.log("yeoooooo");
+    $scope.calcul = function(obj){
+       var result =  obj / $scope.brutannuel / 12 * 0.77  // mois
+        if( result < 12){
+            return result + ' mois';
+        }
+        else if(result > 11){
+            return (result / 12) + ' ans';
+        }
+        else{
+            return 'meeeh'
+        }
+    }
+    $scope.test = 'hello le test';
     $scope.brutannuel = 40000;
-}])     
-console.log("yeoooooo");
+    $scope.prixObjet = 100;
+    $scope.objets = 20;
+}])
