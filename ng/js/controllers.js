5b94ab7943ce320b653fefe6372d686762c5a019
tykaynGT <tykayngt@gmail.com>
2013-06-11 16:50:57 +0200

    calculs adaptatifs

--- ng/js/controllers.js
+++ ng/js/controllers.js
@@ -5,33 +5,35 @@
     $scope.futur = d.getFullYear() + $scope.ans 
     
     $scope.calcul =     function(obj,brutannuel){
-            console.log("brutannuel" + brutannuel);
-      var result =   obj / brutannuel * 0.77  ; // convertir le brut annuel en net
-      
-      console.log("result " + result+' mois');
-      console.log("result " + result*31 +' jours');
+     
+      var netannuel = brutannuel * 0.77  ;
+      var netmensuel = netannuel/12  ;
+      var nethoraire = netmensuel / 151;
+      var result =   obj / netmensuel; 
+        console.log("brutannuel " + obj + " netmensuel " + netmensuel);
+      console.log(obj + "€ = result " + result+' mois');
+      console.log(obj + "€ = result " + result*31 +' jours');
         if( result < 0.5){
-         var h = result *5*31*7
+         var h = result *151;
             return h.toFixed(2)  + ' heures';
         }
         else
         if( result < 1){
-         var jours = result *31
+         var jours = result *31;
             return jours.toFixed(2)  + ' jours';
         }
         else if( result < 12){
             return result.toFixed(2)  + ' mois';
         }
         else if(result > 11){
-             var ans = result / 12 
-         $scope.futur =  parseFloat(d.getFullYear()) + parseFloat(ans.toFixed(1))
+             var ans = result / 12 ;
+         $scope.futur =  parseFloat(d.getFullYear()) + parseFloat(ans.toFixed(1));
             return ans.toFixed(1) + ' ans';
             
         }
         else{
-            return 'meeeh'
+            return 'meeeh';
         }
-        return 'heh'
     }
     
     $scope.millions = function(nombre){
